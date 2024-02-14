07ab604ce2b1c04523880a41e02be296d3f844e8
Dino Source <dinosource.cpp@gmail.com>
2024-02-14 11:13:04 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int myAtoi(std::string s) {
+        int result {};
+
+        // TODO: implement this function (see task_description.md)
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    std::cout << sol.myAtoi("") << '\n';
+}
