cc68ff44a69f8991eef9d5dd8aba84edd479114a
Dino Source <dinosource.cpp@gmail.com>
2024-02-15 22:37:49 -0800

    Add more tests for IsPositive and IsNegative methods

--- tests/solution_test.cpp
+++ tests/solution_test.cpp
@@ -2,24 +2,96 @@
 #include <gtest/gtest.h>
 #include "../src/solution.h"
 
-
-TEST(TestTopic, NumberHasSignPlus) {
+// The following test cases are to test Solution::hasSign method
+TEST(TestTopic, HasSignReturnsTrueIfArgHasPlusSignInTheFront) {
     Solution sol;
     std::string test_data {"+1234"};
     std::ranges::reverse(test_data);
     EXPECT_TRUE(sol.hasSign(test_data));
 }
 
-TEST(TestTopic, NumberHasSignMinus) {
+TEST(TestTopic, HasSignReturnsTrueIfArgHasMinusSignInTheFront) {
     Solution sol;
     std::string test_data {"-1234"};
     std::ranges::reverse(test_data);
     EXPECT_TRUE(sol.hasSign(test_data));
 }
 
-TEST(TestTopic, NumberHasNotAnySign) {
+TEST(TestTopic, HasSignReturnsFalseIfArgHasNoSign) {
     Solution sol;
     std::string test_data {"1234"};
     std::ranges::reverse(test_data);
     EXPECT_FALSE(sol.hasSign(test_data));
+}
+
+TEST(TestTopic, HasSignReturnsFalseIfArgHasMinusSignInTheBack) {
+    Solution sol;
+    std::string test_data {"1234-"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.hasSign(test_data));
+}
+
+TEST(TestTopic, HasSignReturnsFalseIfArgHasPlusSignInTheBack) {
+    Solution sol;
+    std::string test_data {"1234+"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.hasSign(test_data));
+}
+
+TEST(TestTopic, HasSignReturnsFalseIfArgHasMinusSignInTheMiddle) {
+    Solution sol;
+    std::string test_data {"12-34"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.hasSign(test_data));
+}
+
+TEST(TestTopic, HasSignReturnsFalseIfArgHasPlusSignInTheMiddle) {
+    Solution sol;
+    std::string test_data {"12+34"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.hasSign(test_data));
+}
+
+// The following test cases are to test Solution::isPositive method
+TEST(TestTopic, IsPositiveReturnsTrueIfArgHasPlusSignInTheFront) {
+    Solution sol;
+    std::string test_data {"+1234"};
+    std::ranges::reverse(test_data);
+    EXPECT_TRUE(sol.isPositive(test_data));
+}
+
+TEST(TestTopic, IsPositiveReturnsTrueIfArgHasNoSign) {
+    Solution sol;
+    std::string test_data {"1234"};
+    std::ranges::reverse(test_data);
+    EXPECT_TRUE(sol.isPositive(test_data));
+}
+
+TEST(TestTopic, IsPositiveReturnsFalseIfArgHasMinusSignInTheFront) {
+    Solution sol;
+    std::string test_data {"-1234"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.isPositive(test_data));
+}
+
+// The following test cases are to test Solution::isNegative method
+TEST(TestTopic, IsNegativeReturnsTrueIfArgHasMinusSignInTheFront) {
+    Solution sol;
+    std::string test_data {"-1234"};
+    std::ranges::reverse(test_data);
+    EXPECT_TRUE(sol.isNegative(test_data));
+}
+
+TEST(TestTopic, IsNegativeReturnsFalseIfArgHasPlusSignInTheFront) {
+    Solution sol;
+    std::string test_data {"+1234"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.isNegative(test_data));
+}
+
+TEST(TestTopic, IsNegativeReturnsFalseIfArgHasNoSign) {
+    Solution sol;
+    std::string test_data {"1234"};
+    std::ranges::reverse(test_data);
+    EXPECT_FALSE(sol.isNegative(test_data));
 }
