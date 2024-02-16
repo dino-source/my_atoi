ca926d73cb2bc721ffb07588ee5b0f91be74b607
Dino Source <dinosource.cpp@gmail.com>
2024-02-15 23:09:45 -0800

    Add more tests to test fixBounds method

--- tests/solution_test.cpp
+++ tests/solution_test.cpp
@@ -95,3 +95,16 @@ TEST(TestTopic, IsNegativeReturnsFalseIfArgHasNoSign) {
     std::ranges::reverse(test_data);
     EXPECT_FALSE(sol.isNegative(test_data));
 }
+
+// The following test cases are to test Solution::fixBounds method
+TEST(TestTopic, fixBoundsReturnsUpperBoundIfArgIsGreater) {
+    Solution sol;
+    long long test_data {9223372036854775807LL};
+    EXPECT_EQ(sol.fixBounds(test_data, 1), Solution::UPPER_BOUND);
+}
+
+TEST(TestTopic, fixBoundsReturnsLowerBoundIfArgIsLower) {
+    Solution sol;
+    long long test_data {9223372036854775807LL};
+    EXPECT_EQ(sol.fixBounds(test_data, -1), Solution::LOWER_BOUND);
+}
