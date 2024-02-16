5fecda47dcc3beac9ad83e873b7904967e4a72d6
Dino Source <dinosource.cpp@gmail.com>
2024-02-16 13:22:18 -0800

    Add myAtoiTest

---
+++ test/myAtoiTest.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <gtest/gtest.h>
+#include "../src/solution.h"
+
+
+TEST(TestTopic, myAtoiConvertsArgWithoutSignToPositiveNumber) {
+    Solution sol;
+    std::string test_data {"1234"};
+    EXPECT_EQ(sol.myAtoi(test_data), 1234);
+}
+
+TEST(TestTopic, myAtoiConvertsArgWithPlusSignToPositiveNumber) {
+    Solution sol;
+    std::string test_data {"+1234"};
+    EXPECT_EQ(sol.myAtoi(test_data), 1234);
+}
+
+TEST(TestTopic, myAtoiConvertsArgWithMinusSignToNegativeNumber) {
+    Solution sol;
+    std::string test_data {"-1234"};
+    EXPECT_EQ(sol.myAtoi(test_data), -1234);
+}
+
+TEST(TestTopic, myAtoiReturnsZeroIfArgIsNotANumber) {
+    Solution sol;
+    std::string test_data {"AAAA"};
+    EXPECT_EQ(sol.myAtoi(test_data), 0);
+}
+
+TEST(TestTopic, myAtoiReturnsZeroIfArgIsEmptyString) {
+    Solution sol;
+    std::string test_data {""};
+    EXPECT_EQ(sol.myAtoi(test_data), 0);
+}
