eb56a4caf2bcfbaff658fe6b3e8b482acac37547
Dino Source <dinosource.cpp@gmail.com>
2024-03-11 18:40:56 -0700

    Rename local variable to improve readability

--- tests/order_of_magnitude_test.cpp
+++ tests/order_of_magnitude_test.cpp
@@ -4,50 +4,50 @@
 
 TEST(TestTopic, OrderOfMagnitude1) {
     Solution sol;
-    int test_data {0};
+    int number_of_digits {0};
 
     int expected_result {1};
-    int actual_result {static_cast<int>(sol.orderOfMagnitude(test_data))};
+    int actual_result {static_cast<int>(sol.orderOfMagnitude(number_of_digits))};
 
     EXPECT_EQ(expected_result, actual_result);
 }
 
 TEST(TestTopic, OrderOfMagnitude2) {
     Solution sol;
-    int test_data {1};
+    int number_of_digits {1};
 
     int expected_result {1};
-    int actual_result {static_cast<int>(sol.orderOfMagnitude(test_data))};
+    int actual_result {static_cast<int>(sol.orderOfMagnitude(number_of_digits))};
 
     EXPECT_EQ(expected_result, actual_result);
 }
 
 TEST(TestTopic, OrderOfMagnitude3) {
     Solution sol;
-    int test_data {2};
+    int number_of_digits {2};
 
     int expected_result {10};
-    int actual_result {static_cast<int>(sol.orderOfMagnitude(test_data))};
+    int actual_result {static_cast<int>(sol.orderOfMagnitude(number_of_digits))};
 
     EXPECT_EQ(expected_result, actual_result);
 }
 
 TEST(TestTopic, OrderOfMagnitude4) {
     Solution sol;
-    int test_data {3};
+    int number_of_digits {3};
 
     int expected_result {100};
-    int actual_result {static_cast<int>(sol.orderOfMagnitude(test_data))};
+    int actual_result {static_cast<int>(sol.orderOfMagnitude(number_of_digits))};
 
     EXPECT_EQ(expected_result, actual_result);
 }
 
 TEST(TestTopic, OrderOfMagnitude5) {
     Solution sol;
-    int test_data {4};
+    int number_of_digits {4};
 
     int expected_result {1000};
-    int actual_result {static_cast<int>(sol.orderOfMagnitude(test_data))};
+    int actual_result {static_cast<int>(sol.orderOfMagnitude(number_of_digits))};
 
     EXPECT_EQ(expected_result, actual_result);
 }
